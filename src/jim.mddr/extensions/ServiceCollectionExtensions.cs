3039aee20cfd182a7725259dd9b37f52c32c8332
jimovellan <jimovellan@gmail.com>
2025-08-24 10:14:16 +0200

    md file added

--- src/jim.mddr/extensions/ServiceCollectionExtensions.cs
+++ src/jim.mddr/extensions/ServiceCollectionExtensions.cs
@@ -6,41 +6,49 @@ namespace Jim.Mddr.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds the Mddr services to the specified IServiceCollection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assemblies"></param>
+    /// <returns></returns>
     public static IServiceCollection AddMddr(this IServiceCollection services, List<Assembly> assemblies)
     {
         services.AddScoped<ISender, MddrSender>();
 
-
-        var classes = assemblies.SelectMany(a => a.GetTypes())
-            .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
-            .ToList();
-
-            foreach (var cls in classes)
-            {
-                var interfaces = cls.GetInterfaces()
-                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
-                    .ToList();
-
-                foreach (var iface in interfaces)
-                {
-                    services.AddTransient(iface, cls);
-                }
-            }
+        services.Scan(a => a.FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo(typeof(IRequestHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
 
         return services;
     }
 
+
+    /// <summary>
+    /// Adds the Mddr services to the specified IServiceCollection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assembly"></param>
+    /// <returns></returns>
     public static IServiceCollection AddMddr(this IServiceCollection services, Assembly assembly)
     {
         return services.AddMddr(new List<Assembly> { assembly });
     }
-    
+
+    /// <summary>
+    /// Adds the Mddr services to the specified IServiceCollection.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assemblyNames"></param>
+    /// <returns></returns>
+
     public static IServiceCollection AddMddr(this IServiceCollection services, List<string> assemblyNames)
     {
-
-        
         var assemblies = assemblyNames.Select(Assembly.Load).ToList();
         return services.AddMddr(assemblies);
     }
     
+    
+
 }
